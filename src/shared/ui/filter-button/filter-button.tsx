9270fa6d9bbfbedb86914f458ecd97227aa74052
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2025-06-10 15:27:45 +0300

    feat: finished filter button

--- src/shared/ui/filter-button/filter-button.tsx
+++ src/shared/ui/filter-button/filter-button.tsx
@@ -5,7 +5,7 @@ import { BaseButton, type BaseButtonProps } from "../base-button";
 import { CloseIcon } from "@/shared/assets/icons/close-icon";
 
 const filterButtonVariants = cva(
-  "inline-flex items-center justify-center pl-3 pr-2 py-[8px] rounded-[100px] transition-all disabled:opacity-50 border gap-3 text-sm h-[40px]",
+  "inline-flex items-center justify-center px-3 py-2 rounded-[100px] transition-all disabled:opacity-50 border gap-3 text-sm h-[40px]",
   {
     variants: {
       color: {
@@ -21,9 +21,8 @@ const filterButtonVariants = cva(
 );
 
 export interface FilterButtonProps
-  extends BaseButtonProps,
+  extends Omit<BaseButtonProps, "color">,
     VariantProps<typeof filterButtonVariants> {
-  color?: "coral" | "ocean";
   children: React.ReactNode;
 }
 
