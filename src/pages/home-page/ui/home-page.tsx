7a0187962f941d0f2b00c746cea8f990e33ec4d0
Tolik <tolik.rfd@gmail.com>
2025-07-09 20:06:11 +0300

    resolve merge conflict

--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,10 +1,17 @@
-import { ProductList } from "@/entities/product-list";
-import products from "@/entities/product-list/products.json";
+// import { productsService } from "@/shared/api/supabase/services/products/products-service";
+import { InstagramIcon } from "@/shared/assets/icons/instagram-icon";
+import { CustomButton } from "@/shared/ui/custom-button";
+import { IconButton } from "@/shared/ui/icon-button";
+import { Lorem } from "@/shared/ui/lorem";
+import { PageContainer } from "@/shared/ui/page-container";
+import Link from "next/link";
 
 export default async function HomePage() {
   return (
-    <div className="min-h-screen bg-custom-cofe">
-      <ProductList products={products} className="pt-10" />
-    </div>
+    <main>
+      <PageContainer>
+        <Lorem count={70} />
+      </PageContainer>
+    </main>
   );
 }
