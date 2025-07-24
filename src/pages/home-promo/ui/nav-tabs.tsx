22b4e9548fe9df1537abd1d29083a72e515e4f9a
Tolik <tolik.rfd@gmail.com>
2025-07-24 23:11:25 +0300

    feat: add adaptiv

--- src/pages/home-promo/ui/nav-tabs.tsx
+++ src/pages/home-promo/ui/nav-tabs.tsx
@@ -7,12 +7,17 @@ import { usePathname } from "next/navigation";
 
 export const NavTabs = () => {
   const pathname = usePathname();
-  console.log(pathname);
+
   return (
     <Tabs value={pathname || AppRoutes.HOME}>
       <TabsList>
         {MAIN_NAV_ITEMS.map((item) => (
-          <TabsTrigger key={item.value} value={item.value} asChild>
+          <TabsTrigger
+            key={item.value}
+            value={item.value}
+            asChild
+            className="px-4.5 py-4 text-base leading-none lg:px-10 lg:py-4.5 lg:text-xl"
+          >
             <SmartLink href={item.href}>{item.label}</SmartLink>
           </TabsTrigger>
         ))}
